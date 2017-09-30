2e3c7f560cbdd20bfa497defd6c58e807a29a274
kapicaluk <kapicaluk@gmail.com>
2017-09-30 13:53:25 -0400

    passes the specs

--- models/user.js
+++ models/user.js
@@ -2,9 +2,42 @@ const Sequelize = require('sequelize');
 const db = new Sequelize('postgres://localhost:5432/sequelize_practice', { logging: false });
 
 const User = db.define('user', {
-// YOUR CODE HERE...
+    first: {
+        type: Sequelize.STRING,
+    },
+    last: {
+        type: Sequelize.STRING,
+    },
+    age: {
+        type: Sequelize.INTEGER,
+        validate: {
+            // is18: age => {
+            //     if (age < 18) throw new Error('Validation min on age failed')
+            min: 18
+            }
+        },
+    email: {
+        type: Sequelize.STRING,
+        isEmail: true,
+        allowNull: false
+    },
+    bio: {
+        type: Sequelize.TEXT,
+    }
+
+
 }, {
-// ...AND HERE
+    getterMethods: {
+        fullName: function() {
+            return this.first + ' ' + this.last
+        }
+    }
 });
+
+
+//instance method
+User.prototype.haveBirthday = function() {
+    return this.increment('age')
+}
 
 module.exports = User;
